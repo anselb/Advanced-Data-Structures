404e22293e4520aaf93b18991a21be16cee5a581
Ansel Bridgewater <iamansel@gmail.com>
2018-05-07 16:20:33 -0700

    Finished is_empty method for BinaryMinHeap class

--- source/binaryheap.py
+++ source/binaryheap.py
@@ -21,8 +21,8 @@ class BinaryMinHeap(object):
 
     def is_empty(self):
         """Return True if this heap is empty, or False otherwise."""
-        # TODO: Check if empty based on how many items are in the list
-        # ...
+        # Check if empty based on how many items are in the list
+        return True if len(self.items) == 0 else False
 
     def size(self):
         """Return the number of items in this heap."""
