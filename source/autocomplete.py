fbe6adba395988a7d0e0f70e714d05b2f1292adb
Ansel Bridgewater <iamansel@gmail.com>
2018-05-13 02:22:12 -0700

    Finished benchmark function and added fixes that allowed it to work

--- source/autocomplete.py
+++ source/autocomplete.py
@@ -1,3 +1,6 @@
+import time
+
+
 def get_words(filename):
     # Return list: all words in filename
     with open(filename) as f:
@@ -10,26 +13,41 @@ def get_words(filename):
 def autocomplete(words, prefix):
     # Return list: all words given a prefix
     index = binary_search_recursive(array=words, item=prefix)
-    left = index - 1
-    right = index + 1
+    left = index
+    right = index
+    if left > 0:
+        left -= 1
+    if right < len(words) - 1:
+        right += 1
+
     found_words = []
     found_words.append(words[index])
 
-    while words[left].startswith(prefix) or words[right].startswith(prefix):
+    while ((words[left].startswith(prefix) and words[left] not in found_words) or
+            (words[right].startswith(prefix) and words[right] not in found_words)):
         if words[left].startswith(prefix):
             found_words.append(words[left])
         if words[right].startswith(prefix):
             found_words.append(words[right])
-        left -= 1
-        right += 1
+
+        if left > 0:
+            left -= 1
+        if right < len(words) - 1:
+            right += 1
 
     return found_words
 
-def benchmark(num_prefixes):
+def benchmark(words, prefixes):
     # Return float: time to autocomplete all prefixes
     # Generate prefixes by selecting words from the dictionary file and slicing
     # the frst half of their letters -> word[:len(word)//2]
-    pass
+    start_time = time.time()
+    for prefix in prefixes:
+        autocomplete(words, prefix)
+    finish_time = time.time()
+
+    time_to_autocomplete_all = finish_time - start_time
+    return time_to_autocomplete_all
 
 def binary_search_recursive(array, item, left=None, right=None):
     # Starting recursive loop using passed in array to calculate 'right'
@@ -49,14 +67,18 @@ def binary_search_recursive(array, item, left=None, right=None):
         return right
 
     index = (right + left) // 2
-    print(index, array[index], (left, right))
     if item < array[index]:
         return binary_search_recursive(array, item, left, index)
     else:
         return binary_search_recursive(array, item, index, right)
 
 
-words = get_words('/usr/share/dict/words')
-print(words[232769])
+all_words = get_words('/usr/share/dict/words')
+print(all_words[232769])
 # yab - ['yaba', 'yabber', 'yabbi', 'yabble', 'yabby', 'yabu']
-print(autocomplete(words, 'yab'))
+print(autocomplete(all_words, 'yab'))
+
+all_prefixes = set([word[:len(word)//2] for word in all_words])
+all_prefixes.remove('')
+time = benchmark(all_words, all_prefixes)
+print('Took {} seconds to benchmark {} prefixes on {} words'.format(time, len(all_prefixes), len(all_words)))
