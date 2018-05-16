30c11ff32f3b35eab49baba6f73f0367e953648c
Ansel Bridgewater <iamansel@gmail.com>
2018-05-16 14:45:01 -0700

    Fixed all tests to work with a priority queue

--- source/priorityqueue_test.py
+++ source/priorityqueue_test.py
@@ -13,8 +13,8 @@ class PriorityQueueTest(unittest.TestCase):
         assert pq.is_empty() is True
 
     def test_init_with_list(self):
-        pq = PriorityQueue([(3, 'A'), (2, 'B'), (1, 'C')])
-        assert pq.front() == (1, 'C')
+        pq = PriorityQueue([(1, 'A'), (2, 'B'), (3, 'C')])
+        assert pq.front() == (1, 'A')
         assert pq.length() == 3
         assert pq.is_empty() is False
 
@@ -25,43 +25,43 @@ class PriorityQueueTest(unittest.TestCase):
         assert pq.length() == 1
         pq.enqueue('B', 2)
         assert pq.length() == 2
-        assert pq.dequeue() == ((1, 'A'))
+        assert pq.dequeue() == (1, 'A')
         assert pq.length() == 1
-        assert pq.dequeue() == ((2, 'B'))
+        assert pq.dequeue() == (2, 'B')
         assert pq.length() == 0
 
     def test_enqueue(self):
         pq = PriorityQueue()
-        pq.enqueue('A')
-        assert pq.front() == 'A'
+        pq.enqueue('B', 2)
+        assert pq.front() == (2, 'B')
         assert pq.length() == 1
-        pq.enqueue('B')
-        assert pq.front() == 'A'
+        pq.enqueue('A', 1)
+        assert pq.front() == (1, 'A')
         assert pq.length() == 2
-        pq.enqueue('C')
-        assert pq.front() == 'A'
+        pq.enqueue('C', 3)
+        assert pq.front() == (1, 'A')
         assert pq.length() == 3
         assert pq.is_empty() is False
 
     def test_front(self):
         pq = PriorityQueue()
         assert pq.front() is None
-        pq.enqueue('A')
-        assert pq.front() == 'A'
-        pq.enqueue('B')
-        assert pq.front() == 'A'
+        pq.enqueue('A', 1)
+        assert pq.front() == (1, 'A')
+        pq.enqueue('B', 2)
+        assert pq.front() == (1, 'A')
         pq.dequeue()
-        assert pq.front() == 'B'
+        assert pq.front() == (2, 'B')
         pq.dequeue()
         assert pq.front() is None
 
     def test_dequeue(self):
-        pq = PriorityQueue(['A', 'B', 'C'])
-        assert pq.dequeue() == 'A'
+        pq = PriorityQueue([(1, 'A'), (2, 'B'), (3, 'C')])
+        assert pq.dequeue() == (1, 'A')
         assert pq.length() == 2
-        assert pq.dequeue() == 'B'
+        assert pq.dequeue() == (2, 'B')
         assert pq.length() == 1
-        assert pq.dequeue() == 'C'
+        assert pq.dequeue() == (3, 'C')
         assert pq.length() == 0
         assert pq.is_empty() is True
         with self.assertRaises(ValueError):
